3cd3af6a0d8b688257183effb22f47c74c2a486b
NathHK <72760647+NathHK@users.noreply.github.com>
2022-07-26 16:41:23 -0400

    Started work on new OptionsGrid function
    Trying to relocate all calculations of valid destinations for bishops to Bishop.cs. I will comment out the block of code from Manager.cs after I'm done moving code around and merging it with what's already in Bishop.cs.

--- PieceClasses/Bishop.cs
+++ PieceClasses/Bishop.cs
@@ -106,6 +106,11 @@ public class Bishop : PieceBaseClass
 
     public override void OptionsGrid(bool[,] theGrid, int currRank, int currFile)
     {
+
+        Debug.Log("Running old OptionsGrid function in file Bishop.cs!!!");
+
+        // NOTE:
+        // Why did I make a new array if one was received through the function call?
         bool[,] grid = new bool[8,8];
 
         //set counters to current values
@@ -161,6 +166,131 @@ public class Bishop : PieceBaseClass
 
     }
 
+    // pieces : current locations of all pieces on the board
+    // optionsGrid : validity of destinations; starts all false
+    // manager : manager of the current game
+    // currentPlayer : player whose turn it is
+    // piece : the piece to determine the valid destinations for
+    // currRank : current rank of piece
+    // currFile : current file of piece
+    public void OptionsGrid(GameObject[,] pieces, bool[,] optionsGrid, Manager manager, Player currentPlayer, GameObject piece, int currRank, int currFile) 
+    {
+
+        // MOVEMENT RULE:
+        // Bishops can move diagonally in all directions, any number of squares, but cannot jump over other pieces.
+
+        // check LEFT & UP
+        int rank = currRank+1;
+        int file = currFile-1;
+        bool pieceFound = false;
+        int pFoundAt = file;
+        bool validFound = false;
+        int vFoundAt = file;
+
+        while(file >= 0 && file <= 7 && rank <= 7 && rank >= 0) 
+        {
+
+            if(!pieceFound && pieces[rank,file] != null && manager.PieceOwner(pieces[rank,file]) == manager.PieceOwner(piece))
+            {
+                pieceFound = true;
+                pFoundAt = file;
+            }
+            if(!validFound && pieces[rank,file] != null && optionsGrid[rank,file] && manager.PieceOwner(pieces[rank,file]) != "" && manager.PieceOwner(pieces[rank,file]) != manager.PieceOwner(piece)){
+                validFound = true;
+                vFoundAt = file;
+            }
+            if((pieceFound && file < pFoundAt) || (validFound && file < vFoundAt))
+                optionsGrid[rank,file] = false;
+
+            file = file-1;
+            rank = rank+1;
+
+        }
+
+        // check RIGHT & UP
+        file = currFile+1;
+        rank = currRank+1;
+        pieceFound = false;
+        validFound = false;
+        pFoundAt = file;
+        vFoundAt = file;
+
+        while(file <= 7 && file >= 0 && rank <= 7 && rank >= 0) 
+        {
+
+            if(pieces[rank,file] != null && manager.PieceOwner(pieces[rank,file])== manager.PieceOwner(piece))
+            {
+                pieceFound = true;
+                pFoundAt = file;
+            }
+            if(!validFound && pieces[rank,file] != null && optionsGrid[rank,file] && manager.PieceOwner(pieces[rank,file]) != "" && manager.PieceOwner(pieces[rank,file]) != manager.PieceOwner(piece)){
+                validFound = true;
+                vFoundAt = file;
+            }
+            if((pieceFound && file > pFoundAt) || (validFound && file>vFoundAt))
+                optionsGrid[rank,file] = false;
+
+            file = file+1;
+            rank = rank+1;    
+
+        }
+
+        // check LEFT & DOWN
+        file = currFile-1;
+        rank = currRank-1;
+        pieceFound = false;
+        pFoundAt = rank;
+        validFound = false;
+        vFoundAt = rank;
+
+        while(rank <= 7 && rank >= 0 && file >= 0 && file <= 7) 
+        {
+
+            if(pieces[rank,file] != null && manager.PieceOwner(pieces[rank,file])== manager.PieceOwner(piece))
+            {
+                pieceFound = true;
+                pFoundAt = rank;
+            }
+            if(!validFound && pieces[rank,file] != null && optionsGrid[rank,file] && manager.PieceOwner(pieces[rank,file]) != "" && manager.PieceOwner(pieces[rank,file]) != manager.PieceOwner(piece)){
+                validFound = true;
+                vFoundAt = rank;
+            }
+            if((pieceFound && rank>pFoundAt) || (validFound && rank>vFoundAt))
+                optionsGrid[rank,file] = false;
+            rank = rank-1;
+            file = file-1;   
+
+        }
+
+        // check RIGHT & DOWN
+        rank = currRank-1;
+        file = currFile+1;
+        pieceFound = false;
+        pFoundAt = rank;
+        validFound = false;
+        vFoundAt = rank;
+
+        while(rank >= 0 && rank <= 7 && file <= 7 && file >= 0) 
+        {
+
+            if(pieces[rank,file] != null && manager.PieceOwner(pieces[rank,file])== manager.PieceOwner(piece))
+            {
+                pieceFound = true;
+                pFoundAt = rank;
+            }
+            if(!validFound && pieces[rank,file] != null && optionsGrid[rank,file] && manager.PieceOwner(pieces[rank,file]) != "" && manager.PieceOwner(pieces[rank,file]) != manager.PieceOwner(piece)){
+                validFound = true;
+                vFoundAt = rank;
+            }
+            if((pieceFound && rank<pFoundAt) || (validFound && rank<vFoundAt))
+                optionsGrid[rank,file] = false;
+            rank = rank-1;
+            file = file+1;
+
+        }
+
+    }
+
     public override void MovePiece(Vector3 dest, bool attack)
     {
 
@@ -170,17 +300,18 @@ public class Bishop : PieceBaseClass
         if(attack){
 
             if(dest.x > agent.destination.x){
-                //RIGHT & UP
+                // RIGHT & UP
                 if(dest.z > agent.destination.z)
                     stopHere = new Vector3(dest.x - 1.5f, dest.y, dest.z-(1.5f*posZ));
-                //RIGHT & DOWN
+                // RIGHT & DOWN
                 else if(dest.z < agent.destination.z)
                     stopHere = new Vector3(dest.x - 1.5f, dest.y, dest.z+(1.5f*posZ));
             }
             else if(dest.x < agent.destination.x){
-                //LEFT & UP
+                // LEFT & UP
                 if(dest.z > agent.destination.z)
                     stopHere = new Vector3(dest.x + 1.5f, dest.y, dest.z-(1.5f*posZ));
+                // LEFT & DOWN
                 else if(dest.z < agent.destination.z)
                     stopHere = new Vector3(dest.x + 1.5f, dest.y, dest.z+(1.5f*posZ));
             }
